0ef422503b4df06050f1b35438756296dc664945
Alexander Bouma <bouma.a@gmail.com>
2016-07-28 13:55:36 +0200

    modified:   FTC/FTC/__pycache__/settings.cpython-35.pyc 	modified:   FTC/FTC/__pycache__/urls.cpython-35.pyc 	modified:   FTC/FTC/__pycache__/wsgi.cpython-35.pyc 	modified:   FTC/db.sqlite3 	modified:   FTC/main/__pycache__/admin.cpython-35.pyc 	modified:   FTC/main/__pycache__/apps.cpython-35.pyc 	modified:   FTC/main/__pycache__/models.cpython-35.pyc 	modified:   FTC/main/__pycache__/urls.cpython-35.pyc 	modified:   FTC/main/__pycache__/views.cpython-35.pyc 	modified:   FTC/main/models.py 	new file:   FTC/main/models.py~ 	new file:   FTC/main/static/main/user_piecharts/alexandrb.png 	new file:   FTC/main/vuldieshit.py

--- FTC/main/models.py
+++ FTC/main/models.py
@@ -1,3 +1,66 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Modules(models.Modules):
+        AREAS = (
+                ('studie'),
+                ('sociaal'),
+                ('toekomst'),
+                ('studiesociaal'),
+                ('studietoekomst'),
+                ('sociaaltoekomst')
+        )
+
+        gebied = models.CharField(choices=AREAS, default='studie')
+        naam = models.CharField(max_length=50)
+        omschrijving = models.TextField()
+        time = models.IntegerField('Tijdsduur', default=4)
+        kosten = models.IntegerField('Kosten', default=100)
+        baten_vast = models.IntegerField('Vaste baten', default=100)
+        baten_flex = models.IntegerField('Flexibele baten', default=100)
+        experience_vast = models.IntegerField('Vaste exp', default=100)
+        experience_flex = models.IntegerField('Flexibele exp', default=100)
+        factor = models.DecimalField(max_digits=3, decimal_places=1)
+
+
+class Questions(models.Questions):
+        CHOICES = (
+                ('Ja', 'Ja'),
+                ('Nee', 'Nee'),
+                ('WN', 'Weet niet')
+        )
+
+        question = models.TextField()
+        answers = models.CharField(choices=CHOICES, default='WN')
+
+
+class UserProfile(models.Model):
+        user = models.OneToOneField(User)
+
+        # additional information
+        GENDERS = (
+                ('M', 'Man'),
+                ('V', 'Vrouw'),
+        )
+        # General info
+        firstname = models.CharField(max_length=50)
+        lastname = models.CharField(max_length=50)
+        sexe = models.CharField(max_length=1, choices=GENDERS, default='M')
+        bank = models.IntegerField('Bank', default=150)
+        exp_tot = models.IntegerField('Experience totaal', default=0)
+        exp_stud = models.IntegerField('Experience studie', default=0)
+        exp_soc = models.IntegerField('Experience sociaal', default=0)
+        exp_toek = models.IntegerField('Experience toekomst', default=0)
+
+        # Weging voor piechart enzo
+        weging_stud = models.IntegerField('Experience studie', default=40)
+        weging_soc = models.IntegerField('Experience sociaal', default=30)
+        weging_toek = models.IntegerField('Experience toekomst', default=30)
+
+        # relations bitches
+        modules = models.ManyToManyField(Modules)
+        questions = models.ManyToManyField(Questions)
+
+        def __unicode__(self):
+                return self.user.username
